4fac45924f0f204de3f79782625e4a3037edf76a
Zehuan Huang <zewanhuang@126.com>
2022-01-22 11:35:07 +0800

    fix(gh-pages): public-path

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-    runtimeCompiler: true
-}
+    lintOnSave: false,
+    runtimeCompiler: true,
+    publicPath:
+        process.env.NODE_ENV === "production" ? "/vue-running" : "/vue-running",
+    productionSourceMap: false, //关闭生产环境的sourceMap
+};
