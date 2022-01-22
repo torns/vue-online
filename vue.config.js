d55b751fd346750d8f95184f24f3de5b9998bb75
Zehuan Huang <zewanhuang@126.com>
2022-01-22 11:42:44 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,6 @@ module.exports = {
     lintOnSave: false,
     runtimeCompiler: true,
     publicPath:
-        process.env.NODE_ENV === "production" ? "/vue-running" : "/vue-running",
+        process.env.NODE_ENV === "production" ? "/vue-online" : "/vue-online",
     productionSourceMap: false, //关闭生产环境的sourceMap
 };
